a5a257cd746961a18c6df3c1368a9a8fed22f7b4
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-01-26 13:26:27 -0300

    Api gateway filters and interceptors

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
