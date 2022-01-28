352171439d60011407401bdffa389a1c9e3b3d6a
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-01-27 23:34:37 -0300

    User microservice

---
+++ microservice-users/src/user/user.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { UserMessage } from 'src/common/consts';
+import { UserDTO } from './dto/user.dto';
+import { UserService } from './user.service';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @MessagePattern(UserMessage.Create)
+  create(@Payload() userDto: UserDTO) {
+    return this.userService.create(userDto);
+  }
+
+  @MessagePattern(UserMessage.FindAll)
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @MessagePattern(UserMessage.FindOne)
+  findById(@Payload() id: string) {
+    return this.userService.findById(id);
+  }
+
+  @MessagePattern(UserMessage.Update)
+  update(@Payload() payload: any) {
+    return this.userService.update(payload.id, payload.userDto);
+  }
+
+  @MessagePattern(UserMessage.Delete)
+  delete(@Payload() payload: any) {
+    return this.userService.delete(payload.id);
+  }
+}
---
+++ microservice-users/src/user/user.service.ts
@@ -0,0 +1,52 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { UserInterface } from '../common/interfaces/user.interface';
+import { UserDTO } from './dto/user.dto';
+import * as bcrypt from 'bcrypt';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from '../common/models/models';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private readonly model: Model<UserInterface>,
+  ) {}
+
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  }
+
+  async create(userDto: UserDTO): Promise<UserInterface> {
+    const hash = await this.hashPassword(userDto.password);
+    const newUser = new this.model({ ...userDto, password: hash });
+    return await newUser.save();
+  }
+
+  async findAll(): Promise<UserInterface[]> {
+    return await this.model.find();
+  }
+
+  async findById(id: string): Promise<UserInterface> {
+    return await this.model.findById(id);
+  }
+
+  async update(id: string, userDto: UserDTO): Promise<UserInterface> {
+    const hash = await this.hashPassword(userDto.password);
+    const user = { ...userDto, password: hash };
+    return await this.model.findByIdAndUpdate(id, user, { new: true });
+  }
+
+  async delete(id: string) {
+    await this.model.findByIdAndDelete(id);
+    return { status: HttpStatus.OK, message: 'User deleted' };
+  }
+
+  async findByUsername(username: string) {
+    return await this.model.findOne({ username });
+  }
+
+  async checkPassword(password: string, passwordDB: string): Promise<boolean> {
+    return await bcrypt.compare(password, passwordDB);
+  }
+}
